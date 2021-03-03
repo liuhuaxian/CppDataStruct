173ba00360699ca4ac73cf3d0d1d58cf44bfafa0
yanlele <yanlele@chinaredflag.cn>
2021-03-03 15:59:58 +0800

    2021年03月 3日 15:59:58 push 20-数组类的创建-下/

---
+++ 20-数组类的创建-下/DTLib/DynamicArray.h
@@ -0,0 +1,11 @@
+﻿#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+#include"Array.h"
+template<typename T,int N>
+class DynamicArray:public Array<T>
+{
+public:
+    DynamicList();
+};
+
+#endif // DYNAMICLIST_H
---
+++ 20-数组类的创建-下/DTLib/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "StaticArray.h"
+
+using namespace std;
+using namespace DTLib;
+
+int main(){
+    cout << "main begin" << endl;
+    StaticArray<int, 5> a;
+
+    for (int i=0; i<a.length(); ++i){
+        a[i] = i * i;
+    }
+    for (int i=0; i<a.length(); ++i){
+        cout << a[i] << endl;
+    }
+    cout << "---------" << endl;
+
+
+    StaticArray<int, 5> a1;
+    a1 = a;
+    for (int i=0; i<a1.length(); ++i){
+        cout << a[i] << endl;
+    }
+    cout << "*******************" << endl;
+    return 0;
+}
+
