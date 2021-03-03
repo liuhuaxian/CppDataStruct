ad18bb8f164dc6f9751ef9442769fd9494cacec1
yanlele <yanlele@chinaredflag.cn>
2021-03-03 14:46:22 +0800

    2021年03月 3日 14:46:22 push 18-顺序存储线性表的分析/

---
+++ 17-StaticList& DynamicList/DTLib -DynamicList/DynamicList.h
@@ -0,0 +1,65 @@
+﻿#ifndef DYNAMICLIST_H
+#define DYNAMICLIST_H
+#include "SqlList.h"
+#include "Exception.h"
+/*
+类模板
+     申请连续堆空间作为顺序存储空间
+     动态设置顺寻存储空间的大小
+     保证重置顺序存储空间时的异常安全性
+函数异常安全的概念
+     不泄露任何资源
+     不允许破坏数据
+函数异常安全的基本保证
+     如果异常被抛出
+     对象内的任何成员仍然能保持有效状态
+     没有数据的破坏及资源泄漏
+
+*/
+namespace DTLib{
+template <typename T>
+class DynamicList:public SqlList<T>
+{
+protected:
+    int m_capacity;  // 顺序存储空间的大小
+public:
+    ////申请空间
+    DynamicList(int capacity){
+        this->m_array = new T[capacity];
+        if(this->m_array != NULL){
+            this->m_capacity = capacity;
+            this->m_length = 0;//??
+        }else{
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create DynamicList object ...");
+        }
+    }
+    int capacity() const{
+        return m_capacity;
+    }
+    // 重新设置存储空间的大小
+    void resize(int capacity){
+        if (capacity != m_capacity){
+            T *array = new T[capacity];  //注意1 申请新的内存空间，对原内存空间中的数据进行拷贝，以保留原始值。
+            if(array!=0){
+                int length =(this->m_length < capacity)?this->m_length:capacity;
+                for(int i=0;i<length;++i){
+                    array[i] = this->m_array[i];        // 注意 2
+                }
+                T *temp = this->m_array;            // 注意 3
+
+           this->m_array    = array;
+           this->m_length   = length;
+           this->m_capacity = capacity;
+           delete [] temp;                     // 注意 3
+        }else{
+                THROW_EXCEPTION(NoEnoughMemoryException, "No meomry to resize DynamicList object ...");
+            }
+        }
+    }
+    // 归还空间
+    ~DynamicList(){
+        delete [] this->m_array;
+    }
+};
+}
+#endif // DYNAMICLIST_H
---
+++ 17-StaticList& DynamicList/DTLib -DynamicList/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include "StaticList.h"
+#include "DynamicList.h"
+using namespace std;
+using namespace DTLib;
+
+int main(){
+    cout << "main begin" << endl;
+
+    DynamicList<int>sl(5);
+
+
+    for (int i = 0; i < sl.capacity(); ++i){
+         sl.insert(0, i);
+    }
+
+     for (int i = 0; i< sl.length(); ++i){
+        cout << sl[i] << endl;
+     }
+
+     cout << "--------------" << endl;
+     sl[0] *= sl[0];
+
+        for (int i = 0; i< sl.length(); ++i)
+        {
+            cout << sl[i] << endl;
+        }
+
+        cout << "--------------" << endl;
+        try
+            {
+               sl[5] = 5;
+            }
+            catch (const Exception &e)
+            {
+                cout << e.message() << endl;
+                cout << e.location() << endl;
+
+                sl.resize(10);
+                sl.insert(5, 50);
+            }
+
+            for (int i = 0; i< sl.length(); ++i)
+            {
+                cout << sl[i] << endl;
+            }
+
+            cout << "--------------" << endl;
+
+            sl.resize(3);
+            for (int i = 0; i< sl.length(); ++i)
+            {
+                cout << sl[i] << endl;
+            }
+
+            cout << "main end" << endl;
+
+    return 0;
+}
+
