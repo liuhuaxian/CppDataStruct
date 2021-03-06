80abad5ecb00dd4626109710d9216a87cd295a99
yll <yll@464516186@qq.com>
2021-03-06 21:06:59 +0800

    2021年03月 6日 21:06:59 push 18-shunxucunchuxianxingbiaodefenxi/

--- 18-shunxucunchuxianxingbiaodefenxi/DTLib/main.cpp
+++ 18-shunxucunchuxianxingbiaodefenxi/DTLib/main.cpp
@@ -8,7 +8,7 @@ int main(){
     cout << "main begin" << endl;
     DynamicList<int>l(5);
     for(int i=0;i<l.capacity();i++){
-        l.insert(i);
+        l.insert(i);//线性表必须先插入元素，才能使用操作符[]访问元素
     }
     for(int i=0;i<l.length();i++){
         cout << l[i] << endl;
