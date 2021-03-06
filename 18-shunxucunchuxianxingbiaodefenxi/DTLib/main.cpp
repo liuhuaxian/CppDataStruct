41d06eddd42ff3c36e171616a71e11218283c682
yll <yll@464516186@qq.com>
2021-03-06 21:12:35 +0800

    2021年03月 6日 21:12:35 push 18-shunxucunchuxianxingbiaodefenxi/

--- 18-shunxucunchuxianxingbiaodefenxi/DTLib/main.cpp
+++ 18-shunxucunchuxianxingbiaodefenxi/DTLib/main.cpp
@@ -8,7 +8,10 @@ int main(){
     cout << "main begin" << endl;
     DynamicList<int>l(5);
     for(int i=0;i<l.capacity();i++){
-        l.insert(i);//线性表必须先插入元素，才能使用操作符[]访问元素
+        //顺序存储结构线性表提供额数组操作符重载，通过重载能够快捷方便的获取目标位置处的数据元素
+        //在具体使用方式上类似数组，但是由于本质不同，不能够代替数组使用
+
+        l.insert(i);//线性表必须先插入元素，才能使用操作符[]访问元素 线性表不是数组
     }
     for(int i=0;i<l.length();i++){
         cout << l[i] << endl;
